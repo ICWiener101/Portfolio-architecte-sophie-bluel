4f53c63fa9b4eb056b62855961aa7d88659b38d5
ICWiener101 <plamen.stoyanov87@gmail.com>
2023-04-04 19:19:45 +0300

    Added commentary in login

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,47 +1,42 @@
-
-
 const loginForm = document.getElementById('loginForm');
 loginForm.addEventListener('submit', onLogin);
 
+//login function
+async function onLogin(event) {
+    event.preventDefault();
+    const url = 'http://localhost:5678/api/users/login';
+    const formData = new FormData(loginForm);
 
-async function onLogin(event){
-	event.preventDefault();
-	const url = 'http://localhost:5678/api/users/login';
-	const formData = new FormData(loginForm);
+    const email = formData.get('email').trim();
+    const password = formData.get('password').trim();
 
+    const user = { email, password };
+    if (window.sessionStorage.getItem('token') === null) {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            });
+            if (response.status !== 200) {
+                const error = await response.json();
+                throw new Error(`Error: ${error.message}`);
+            }
 
-	const email = formData.get('email').trim();
-	const password = formData.get('password').trim();
+            const result = await response.json();
 
-	const user = {email, password};
-	if( window.sessionStorage.getItem('token') === null){
-		try{
-			const response = await fetch(url, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(user)
-			});
-			if( response.status !== 200){
-				const error = await response.json();
-				throw new Error(`Error: ${error.message}`);
-			}
+            const token = result.token;
 
-			const result = await response.json();
+            window.sessionStorage.setItem('token', token);
+            window.location = './index.html';
+        } catch (err) {
 
-			const token = result.token;
-
-			window.sessionStorage.setItem('token', token);
-			window.location = './index.html';
-		}catch(err){
-
-			alert('Wrong email or password!');
-		}
-	}else{
-		alert('User is already logged in!');
-	}
+            alert('Wrong email or password!');
+        }
+    } else {
+        alert('User is already logged in!');
+    }
 
 }
-
-
